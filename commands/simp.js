a086a2b1b062deef9b33580dab47a4f3190b8f3d
FahadAwanKamra <91179741+FahadAwanKamra@users.noreply.github.com>
2025-05-25 19:08:22 +0500

    Update simp.js

--- commands/simp.js
+++ commands/simp.js
@@ -37,8 +37,8 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029Vb0bHpc0G0XmMnX7H91n@newsletter',
+                    newsletterName: '𝙇𝙀𝙊 𝙏𝙀𝘾𝙃',
                     serverMessageId: -1
                 }
             }
@@ -52,8 +52,8 @@ async function simpCommand(sock, chatId, quotedMsg, mentionedJid, sender) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029Vb0bHpc0G0XmMnX7H91n@newsletter',
+                    newsletterName: '𝙇𝙀𝙊 𝙏𝙀𝘾𝙃',
                     serverMessageId: -1
                 }
             }
