a19ea602bda74b729f0315b01dce49eef89c1f03
FahadAwanKamra <91179741+FahadAwanKamra@users.noreply.github.com>
2025-05-25 19:01:58 +0500

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '0029Vb0bHpc0G0XmMnX7H91n@newsletter',
+                    newsletterName: '𝙇𝙀𝙊 𝙏𝙀𝘾𝙃',
                     serverMessageId: -1
                 }
             }
